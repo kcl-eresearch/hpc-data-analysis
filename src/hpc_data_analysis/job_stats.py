1f709493b2fe27efda6c33223929a882761db0d5
Liz Ing-Simmons <liz.ing-simmons@kcl.ac.uk>
2026-08-25 14:42:22 +0100

    feat: output job start and end times

--- src/hpc_data_analysis/job_stats.py
+++ src/hpc_data_analysis/job_stats.py
@@ -57,6 +57,8 @@ def write_csv_row(job, outfile, include_faculty=False):
         str(job["state"]),
         str(job["exit_code"]),
         "1" if job["is_success"] else "0",
+        format_value(job["time_start"]),
+        format_value(job["time_end"]),
         format_value(job["elapsed_sec"]),
         format_value(job["wait_sec"]),
         format_value(job["timelimit_sec"]),
